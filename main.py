e47fbea03c742be670122eb77c6bcc478e17f854
Yuta Tokitake <toki211@gmail.com>
2014-10-08 15:50:24 +0900

    refresh Procfile

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
+if "DATABASE_UR" not in os.environ:
+    os.environ["DATABASE_URL"] = "postgres://npqgmqqpvptjop:UeimxyYhifOcl-0h98tf6yh4Qf@ec2-107-21-226-77.compute-1.amazonaws.com:5432/dc6lkrdhlmmc4d";
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 db = SQLAlchemy(app)
 
 @app.route("/")
-def main():
-    return render_template('index.html')
+def index():
+    return "aaaa";
+    #return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
